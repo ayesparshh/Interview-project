99955414ba83293232dc0e2179a8bb1198fd74b8
ayesparshh <sparshgupta1001@gmail.com>
2024-07-27 21:03:13 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ayesparshh
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"interview-project/db"
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func init() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Warn().Err(err).Msg("No .env file found")
+	}
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+}
+
+func main() {
+
+	wait := make(chan bool)
+
+	DatabaseUrl := os.Getenv("DATABASE_URL")
+
+	DB := db.New(DatabaseUrl)
+	DB.Seed()
+
+}
